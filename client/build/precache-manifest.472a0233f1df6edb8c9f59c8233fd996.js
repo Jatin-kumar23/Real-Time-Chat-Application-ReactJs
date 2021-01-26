f8c42f2ce62748aaff38fd54631751dc1ea2415a
jatin <jatinkumar992001@gmail.com>
2021-01-26 15:40:37 +0530

    initial commit

---
+++ client/build/precache-manifest.472a0233f1df6edb8c9f59c8233fd996.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa24c58d903532debb8707bae1e68c21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "756489f4edc8363d20c1",
+    "url": "/static/css/main.370544e5.chunk.css"
+  },
+  {
+    "revision": "b0dde1762c906854fcdc",
+    "url": "/static/js/2.6936dd8d.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.6936dd8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "756489f4edc8363d20c1",
+    "url": "/static/js/main.8ada8c21.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
